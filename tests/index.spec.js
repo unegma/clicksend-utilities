847678375e632cdb7cbc8b8bc18fdd0cd580b242
Timothy Coleman <timhc22@gmail.com>
2020-10-31 01:05:26 +0000

    Add SMS utilities and test

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -5,13 +5,14 @@ const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 require('dotenv').config();
 const nock = require('nock');
-const ClickSendUtilities = require('../lib/ClickSendUtilities');
+const { ClickSendUtilities, SMSUtilities } = require('../lib');
 const ClickSendIntegrationError = require('../lib/errors/ClickSendIntegrationError');
 
 describe('ClickSend Utilities Test', () => {
-  let cSUtilities;
+  let cSUtilities; let smsUtilities;
   beforeEach(function() {
     cSUtilities = new ClickSendUtilities(process.env.CLICKSEND_USER, process.env.CLICKSEND_API_KEY, process.env.SLACK_ERROR_LOG);
+    smsUtilities = new SMSUtilities(process.env.CLICKSEND_USER, process.env.CLICKSEND_API_KEY, process.env.SLACK_ERROR_LOG);
   });
 
   afterEach(function() {
@@ -29,10 +30,16 @@ describe('ClickSend Utilities Test', () => {
     expect(error.message).to.equal('Error');
   });
 
-  // live test
+  // live test (will get real account)
   it('should get my account', async () => {
     let result = await cSUtilities.getFromCS('/account');
     expect(result.data).to.include.keys('username');
+  });
+
+  // live test using test account mobile number +61411111111
+  it('should send a message to test account', async () => {
+    let result = await smsUtilities.sendSMS('+61411111111', 'Hello');
+    expect(result['response_msg']).to.equal('Messages queued for delivery.');
   });
 
 });
