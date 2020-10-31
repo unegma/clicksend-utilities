67668dd011cdded4f458429117477370b5a8ba8c
Timothy Coleman <timhc22@gmail.com>
2020-10-31 00:41:21 +0000

    Add functionality and test for getting user
    Also add some extra test classes

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,20 +3,21 @@ const expect = chai.expect;
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 const nock = require('nock');
 const ClickSendUtilities = require('../lib/ClickSendUtilities');
 const ClickSendIntegrationError = require('../lib/errors/ClickSendIntegrationError');
 
 describe('ClickSend Utilities Test', () => {
+  let cSUtilities;
   beforeEach(function() {
+    cSUtilities = new ClickSendUtilities(process.env.CLICKSEND_USER, process.env.CLICKSEND_API_KEY, process.env.SLACK_ERROR_LOG);
   });
 
   afterEach(function() {
   });
 
   it('should create an instance of ClickSendUtilities', () => {
-    const cSUtilities = new ClickSendUtilities(process.env.CLICKSEND_USER, process.env.CLICKSEND_API_KEY, process.env.SLACK_ERROR_LOG);
     expect(cSUtilities).to.be.instanceOf(ClickSendUtilities);
     expect(() => {
       cSUtilities.throwError('Message')
@@ -27,4 +28,11 @@ describe('ClickSend Utilities Test', () => {
     const error = new ClickSendIntegrationError('Error');
     expect(error.message).to.equal('Error');
   });
+
+  // live test
+  it('should get my account', async () => {
+    let result = await cSUtilities.getFromCS('/account');
+    expect(result.data).to.include.keys('username');
+  });
+
 });
